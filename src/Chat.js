27ee6e9cb69067f1437db006647210c036a17e60
aradhya-gupta <aradhya.gupta0308@gmail.com>
2021-04-03 15:41:31 +0530

    Add router to rooms to show chats of particular room

--- src/Chat.js
+++ src/Chat.js
@@ -2,19 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 import { AttachFile, SearchOutlined, InsertEmoticon } from "@material-ui/icons"
-import  MicIcon from "@material-ui/icons/Mic"
+import MicIcon from "@material-ui/icons/Mic"
+import { useParams } from "react-router-dom"
 import './Chat.css';
+import db from './firebase';
 
 function Chat() {
     const [seed, setSeed] = useState("");
-    const [input, setInput] = useState(""); 
+    const [input, setInput] = useState("");
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState("");
+
+    useEffect(()=>{
+        if (roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
+                setRoomName(snapshot.data().name)
+            ))
+        }
+    }, [roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5030));
-    }, []);
+    }, [roomId]);
 
-    const sendMessage = (e) =>{
-        e.preventDefault(); 
+    const sendMessage = (e) => {
+        e.preventDefault();
         setInput("");
     }
 
@@ -24,7 +36,7 @@ function Chat() {
                 <Avatar src={`https://avatars.dicebear.com/api/avataaars/${seed}.svg`} />
 
                 <div className="chat__headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at ...</p>
                 </div>
 
@@ -53,14 +65,14 @@ function Chat() {
 
             <div className="chat__footer">
                 <IconButton>
-                    <InsertEmoticon/>
+                    <InsertEmoticon />
                 </IconButton>
                 <form>
-                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message"/>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message" />
                     <button onClick={sendMessage} type="submit">Send a message</button>
                 </form>
                 <IconButton>
-                    <MicIcon/>
+                    <MicIcon />
                 </IconButton>
             </div>
         </div>
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,33 +1,36 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './SidebarChat.css'
-import {Avatar} from "@material-ui/core"
+import { Avatar } from "@material-ui/core"
 import db from './firebase'
+import { Link } from 'react-router-dom';
 
-export default function SidebarChat({id, name, addNewChat}) {
-    const [seed, setSeed]  = useState(""); 
+export default function SidebarChat({ id, name, addNewChat }) {
+    const [seed, setSeed] = useState("");
 
     useEffect(() => {
-        setSeed(Math.floor(Math.random()*4000));
+        setSeed(Math.floor(Math.random() * 4000));
     }, [])
 
-    const createChat = ()=>{
-        const roomname = prompt("Please enter room name."); 
-        if(roomname){
+    const createChat = () => {
+        const roomname = prompt("Please enter room name.");
+        if (roomname) {
             db.collection('rooms').add({
-                name: roomname, 
+                name: roomname,
             });
         }
     }
 
     return !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/avataaars/${seed}.svg`}/>
-            <div className="sidebarChat__info">
-                <h2>{name}</h2>
-                <p>Last message.....</p>
+        <Link to={`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/avataaars/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>Last message.....</p>
+                </div>
             </div>
-        </div>
-    ):(
+        </Link>
+    ) : (
         <div onClick={createChat} className="sidebarChat">
             <h2>Add new chat!</h2>
         </div>
