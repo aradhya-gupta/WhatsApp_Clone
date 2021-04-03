283a9489037126fe83413c7452dfa6ad81462022
aradhya-gupta <aradhya.gupta0308@gmail.com>
2021-04-03 14:04:49 +0530

    Add input message box in the chats section

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from "@material-ui/icons/MoreVert"
-import { AttachFile, SearchOutlined } from "@material-ui/icons"
+import { AttachFile, SearchOutlined, InsertEmoticon } from "@material-ui/icons"
+import  MicIcon from "@material-ui/icons/Mic"
 import './Chat.css';
 
 function Chat() {
-    const [seed, setSeed] = useState('');
+    const [seed, setSeed] = useState("");
+    const [input, setInput] = useState(""); 
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5030));
     }, []);
+
+    const sendMessage = (e) =>{
+        e.preventDefault(); 
+        setInput("");
+    }
 
     return (
         <div className="chat">
@@ -45,7 +52,16 @@ function Chat() {
             </div>
 
             <div className="chat__footer">
-
+                <IconButton>
+                    <InsertEmoticon/>
+                </IconButton>
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message"/>
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <IconButton>
+                    <MicIcon/>
+                </IconButton>
             </div>
         </div>
     )
